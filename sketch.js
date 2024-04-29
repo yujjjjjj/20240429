2fd285a8dcfb9dda068511ef1b6d39fba17d710a
yujjjjjj <150640274+yujjjjjj@users.noreply.github.com>
2024-04-29 10:38:58 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -11,24 +11,27 @@ function setup() {
   captureGraphics.translate(capture_width,0)
   captureGraphics.scale(-1,1)
   capture.hide()
+  //---選鈕的介面
 }
 
 function draw() {
   background(220);
   noStroke()
+  span = 5+map(mouseX,0,width,0,20)
   push()
    translate(width/2-capture_width/2, height/2-capture_height/2) //把原點移到width/2-capture_width/2, height/2-capture_height/2
    captureGraphics.image(capture,0, 0) //在(0,0)
    for(var x= 0;x<captureGraphics.width;x=x+span){
-    for(var y=0;y<captureGraphics.height ; y=y+span){
-      var pixel = captureGraphics.height(x,y)
-      fill(pixel)
-      rect(x,y,span)
+     for(var y=0;y<captureGraphics.height ; y=y+span){
+       var pixel = captureGraphics.get(x,y)
+       fill(pixel)
+       rect(x,y,span)
+       // ellipse(x,y,span)
 
+
+      }
 
     }
-
-   }
   pop()
 
 }

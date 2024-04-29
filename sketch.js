cb1112bf955ca765ae7a5e5939c70a83e971aab4
yujjjjjj <150640274+yujjjjjj@users.noreply.github.com>
2024-04-29 11:50:34 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -14,17 +14,21 @@ function setup() {
 
   //選鈕的介面
   var radioElement = createRadio();
-  radioElement.position(width/2-300,20)
+  radioElement.position(width/2-250,30)
   radioElement.option("方塊")
   radioElement.option("圓圈")
+  radioElement.option("亮度")
+  radioElement.option("紅底")
+  radioElement.option("文字")
   radioElement.style("color","#fff")
-  radioElement.style("font-size","30px")
+  radioElement.style("font-size","50px")
 }
 
 function draw() {
-  background(220);
+  background(0);
   noStroke()
-  span = 5+map(mouseX,0,width,0,20)
+ // span = 5+map(mouseX,0,width,0,20)
+ // span = 15
   push()
    translate(width/2-capture_width/2, height/2-capture_height/2) //把原點移到width/2-capture_width/2, height/2-capture_height/2
    captureGraphics.image(capture,0, 0) //在(0,0)
@@ -32,19 +36,45 @@ function draw() {
      for(var y=0;y<captureGraphics.height ; y=y+span){
        var pixel = captureGraphics.get(x,y)
        fill(pixel)
-       if(radioElement.value()=="方塊"){
+       if(radioElement.value()=="方塊" || radioElement.value()==""){
         rect(x,y,span)
        }
        if(radioElement.value()=="圓圈"){
-        ellipse(x,y,span)
+         ellipse(x,y,span)
        }
-       // ellipse(x,y,span)
+       if(radioElement.value()=="亮度"){
+         bk = (pixel[0]+pixel[1]+pixel[2]) /3
+         fill(bk)
+         // ellipse(x,y,span) //span代表圓圈的直徑
+         ellipse(x,y,span*map(bk,0,255,0,1)) //透過map指令，bk在0~255之間，依
 
+       if(radioElement.value()=="紅底"){
+          colorMode(HSB)
+
+          fill(pixel[0],80,80)
+          rotate(pixel[0]/100)
+          rectMode(CENTER)
+          rect(x,y,span*0.6)
+          fill(0)
+          ellipse(0,0,10)
+
+       if(radioElement.value()=="文字"){
+            const density = 'Ñ@#W$9876543210?!abc;:+=-,._ ';
+            let txt = "一二三四五田雷電龕龘"
+            bk = (pixel[0]+pixel[1]+pixel[2])/3
+            let bkId = int(map(bk, 0, 255, 9, 0))
+            textSize(span)
+            textStyle(BOLD)
+            text(txt[bkId],x,y)
+
+
+          }
+       }
 
       }
 
     }
   pop()
 
+  }
 }
-
